58aecac90737eb4f2a44454b0685b8b80c790a82
Tobias Grothe <mail@tobias-1.de>
2025-01-03 13:30:40 +0100

    Add CI (#9)
    * Add CI
    
    * Add workflows
    
    * spotlessApply
    
    * Fix spotbugs errors

--- src/main/java/Box.java
+++ src/main/java/Box.java
@@ -1,288 +1,297 @@
 import java.util.Arrays;
 
 public class Box {
-    private final int len = 5;
-    private final int empty = -1;
-    private final int maxCandidates;
-    private final boolean withMagicCuboid;
-    private final int[][][] solution = new int[len][len][len];
-    private final int[][] candidates = {
-            {2, 2, 3},
-            {1, 2, 4},
-            {1, 1, 1},
-    };
-    private final int[][] randomCandidates = {
-            {0, 1, 2},
-            {0, 2, 1},
-            {1, 0, 2},
-            {1, 2, 0},
-            {2, 0, 1},
-            {2, 1, 0},
-    };
-    private final int[] available = new int[candidates.length];
-    private final int[] xyz = new int[3];
-    private int freeSpace = len * len * len;
+  private static final int len = 5;
+  private static final int empty = -1;
+  private final int maxCandidates;
+  private final boolean withMagicCuboid;
+  private final int[][][] solution = new int[len][len][len];
+  private final int[][] candidates = {
+    {2, 2, 3},
+    {1, 2, 4},
+    {1, 1, 1},
+  };
+  private final int[][] randomCandidates = {
+    {0, 1, 2},
+    {0, 2, 1},
+    {1, 0, 2},
+    {1, 2, 0},
+    {2, 0, 1},
+    {2, 1, 0},
+  };
+  private final int[] available = new int[candidates.length];
+  private final int[] xyz = new int[3];
+  private int freeSpace = len * len * len;
 
-    public Box(final int maxCandidates, final boolean withMagicCuboid) {
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < len; j++) {
-                for (int k = 0; k < len; k++) {
-                    solution[i][j][k] = empty;
-                }
-            }
+  public Box(final int maxCandidates, final boolean withMagicCuboid) {
+    for (int i = 0; i < len; i++) {
+      for (int j = 0; j < len; j++) {
+        for (int k = 0; k < len; k++) {
+          solution[i][j][k] = empty;
         }
-
-        this.maxCandidates = maxCandidates;
-        Arrays.fill(available, maxCandidates);
-
-        this.withMagicCuboid = withMagicCuboid;
-        if (withMagicCuboid) {
-            // insert magic cuboid
-            solution[2][2][2] = getIndexToInsert(2);
-            available[2]--;
-            freeSpace--;
-        }
+      }
     }
 
-    private void fillCoordinates(final int dir, final int x, final int y, final int z, final int i, final int j, final int k) {
-        xyz[0] = x;
-        xyz[1] = y;
-        xyz[2] = z;
-        switch (dir) {
-            case 0 -> {
-                xyz[0] += i;
-                xyz[1] += j;
-                xyz[2] += k;
-            }
-            case 1 -> {
-                xyz[0] += i;
-                xyz[1] += k;
-                xyz[2] += j;
-            }
-            case 2 -> {
-                xyz[0] += j;
-                xyz[1] += i;
-                xyz[2] += k;
-            }
-            case 3 -> {
-                xyz[0] += j;
-                xyz[1] += k;
-                xyz[2] += i;
-            }
-            case 4 -> {
-                xyz[0] += k;
-                xyz[1] += i;
-                xyz[2] += j;
-            }
-            case 5 -> {
-                xyz[0] += k;
-                xyz[1] += j;
-                xyz[2] += i;
-            }
-        }
-    }
+    this.maxCandidates = maxCandidates;
+    Arrays.fill(available, maxCandidates);
 
-    private boolean isInsertable(final int c, final int x, final int y, final int z, final int dir) {
-        if (available[c] <= 0) {
+    this.withMagicCuboid = withMagicCuboid;
+    if (withMagicCuboid) {
+      // insert magic cuboid
+      solution[2][2][2] = getIndexToInsert(2);
+      available[2]--;
+      freeSpace--;
+    }
+  }
+
+  private void fillCoordinates(
+      final int dir, final int x, final int y, final int z, final int i, final int j, final int k) {
+    xyz[0] = x;
+    xyz[1] = y;
+    xyz[2] = z;
+    switch (dir) {
+      case 0 -> {
+        xyz[0] += i;
+        xyz[1] += j;
+        xyz[2] += k;
+      }
+      case 1 -> {
+        xyz[0] += i;
+        xyz[1] += k;
+        xyz[2] += j;
+      }
+      case 2 -> {
+        xyz[0] += j;
+        xyz[1] += i;
+        xyz[2] += k;
+      }
+      case 3 -> {
+        xyz[0] += j;
+        xyz[1] += k;
+        xyz[2] += i;
+      }
+      case 4 -> {
+        xyz[0] += k;
+        xyz[1] += i;
+        xyz[2] += j;
+      }
+      case 5 -> {
+        xyz[0] += k;
+        xyz[1] += j;
+        xyz[2] += i;
+      }
+      default -> throw new IllegalArgumentException("Invalid direction: " + dir);
+    }
+  }
+
+  private boolean isInsertable(final int c, final int x, final int y, final int z, final int dir) {
+    if (available[c] <= 0) {
+      return false;
+    }
+    int[] a = candidates[c];
+    for (int i = 0; i < a[0]; i++) {
+      for (int j = 0; j < a[1]; j++) {
+        for (int k = 0; k < a[2]; k++) {
+          fillCoordinates(dir, x, y, z, i, j, k);
+          for (int l : xyz) {
+            if (l < 0 || l >= len) {
+              return false;
+            }
+          }
+          if (solution[xyz[0]][xyz[1]][xyz[2]] != empty) {
             return false;
+          }
         }
-        int[] a = candidates[c];
-        for (int i = 0; i < a[0]; i++) {
-            for (int j = 0; j < a[1]; j++) {
-                for (int k = 0; k < a[2]; k++) {
-                    fillCoordinates(dir, x, y, z, i, j, k);
-                    for (int l : xyz) {
-                        if (l < 0 || l >= len) {
-                            return false;
-                        }
-                    }
-                    if (solution[xyz[0]][xyz[1]][xyz[2]] != empty) {
-                        return false;
-                    }
-                }
-            }
+      }
+    }
+    return true;
+  }
+
+  private int getIndexToInsert(final int c) {
+    return c * maxCandidates + (maxCandidates - available[c]);
+  }
+
+  private void insert1(final int c, final int x, final int y, final int z, final int dir) {
+    int toInsert = getIndexToInsert(c);
+    int[] a = candidates[c];
+    for (int i = 0; i < a[0]; i++) {
+      for (int j = 0; j < a[1]; j++) {
+        for (int k = 0; k < a[2]; k++) {
+          fillCoordinates(dir, x, y, z, i, j, k);
+          solution[xyz[0]][xyz[1]][xyz[2]] = toInsert;
+          freeSpace--;
         }
-        return true;
+      }
     }
+    available[c]--;
+  }
 
-    private int getIndexToInsert(final int c) {
-        return c * maxCandidates + (maxCandidates - available[c]);
+  private boolean insert(final int c, final int x, final int y, final int z, final int dir) {
+    if (isInsertable(c, x, y, z, dir)) {
+      insert1(c, x, y, z, dir);
+      return true;
     }
+    return false;
+  }
 
-    private void insert1(final int c, final int x, final int y, final int z, final int dir) {
-        int toInsert = getIndexToInsert(c);
-        int[] a = candidates[c];
-        for (int i = 0; i < a[0]; i++) {
-            for (int j = 0; j < a[1]; j++) {
-                for (int k = 0; k < a[2]; k++) {
-                    fillCoordinates(dir, x, y, z, i, j, k);
-                    solution[xyz[0]][xyz[1]][xyz[2]] = toInsert;
-                    freeSpace--;
-                }
-            }
+  private void remove(final int c, final int x, final int y, final int z, final int dir) {
+    int[] a = candidates[c];
+    for (int i = 0; i < a[0]; i++) {
+      for (int j = 0; j < a[1]; j++) {
+        for (int k = 0; k < a[2]; k++) {
+          fillCoordinates(dir, x, y, z, i, j, k);
+          solution[xyz[0]][xyz[1]][xyz[2]] = empty;
+          freeSpace++;
         }
-        available[c]--;
+      }
     }
+    available[c]++;
+  }
 
-    private boolean insert(final int c, final int x, final int y, final int z, final int dir) {
-        if (isInsertable(c, x, y, z, dir)) {
-            insert1(c, x, y, z, dir);
-            return true;
-        }
-        return false;
+  private boolean isSolved() {
+    return freeSpace == 0;
+  }
+
+  private boolean isProbableSolvable(final int level) {
+    if (freeSpace <= 0) {
+      return false;
     }
-
-    private void remove(final int c, final int x, final int y, final int z, final int dir) {
-        int[] a = candidates[c];
-        for (int i = 0; i < a[0]; i++) {
-            for (int j = 0; j < a[1]; j++) {
-                for (int k = 0; k < a[2]; k++) {
-                    fillCoordinates(dir, x, y, z, i, j, k);
-                    solution[xyz[0]][xyz[1]][xyz[2]] = empty;
-                    freeSpace++;
-                }
-            }
-        }
-        available[c]++;
-    }
-
-    private boolean isSolved() {
-        return freeSpace == 0;
-    }
-
-    private boolean isProbableSolvable(final int level) {
-        if (freeSpace <= 0) {
+    int i = level - 1;
+    if (i >= 0) {
+      // for (int i = 0; i < level; i++) {
+      for (int j = 0; j < len; j++) {
+        for (int k = 0; k < len; k++) {
+          if (solution[i][j][k] == empty) {
             return false;
+          }
         }
-        int i = level - 1;
-        if (i >= 0) {
-            //for (int i = 0; i < level; i++) {
-            for (int j = 0; j < len; j++) {
-                for (int k = 0; k < len; k++) {
-                    if (solution[i][j][k] == empty) {
-                        return false;
-                    }
-                }
-            }
-            //}
-        }
-        return true;
+      }
+      // }
+    }
+    return true;
+  }
+
+  private boolean solve(final int index, final boolean nextRandom) {
+    if (index >= len * len * len) {
+      return isSolved();
+    }
+    final int i = index / len / len;
+    final int j = index / len % len;
+    final int k = index % len;
+    if (!isProbableSolvable(i)) {
+      return false;
     }
 
-    private boolean solve(final int index, final boolean nextRandom) {
-        if (index >= len * len * len) {
-            return isSolved();
-        }
-        final int i = index / len / len;
-        final int j = index / len % len;
-        final int k = index % len;
-        if (!isProbableSolvable(i)) {
-            return false;
-        }
+    if (withMagicCuboid && i == 2 && j == 2 && k == 2) {
+      // skip this step
+      return solve(index + 1, nextRandom);
+    }
 
-        if (withMagicCuboid && i == 2 && j == 2 && k == 2) {
-            // skip this step
-            return solve(index + 1, nextRandom);
-        }
-
-        if (nextRandom) {
-            int r = (int) (Math.random() * randomCandidates.length);
-            for (int ri = 0; ri < candidates.length; ri++) {
-                int current = randomCandidates[r][ri];
-                if (current == 2) {
-                    // take an abbreviation
-                    if (insert(current, i, j, k, 0)) {
-                        if (isSolved()) {
-                            return true;
-                        }
-                        if (isProbableSolvable(i)) {
-                            if (solve(index + 1, true)) {
-                                return true;
-                            }
-                        }
-                        remove(current, i, j, k, 0);
-                    }
-                } else {
-                    // take all directions
-                    for (int d = 0; d < 6; d++) {
-                        if (insert(current, i, j, k, d)) {
-                            if (isSolved()) {
-                                return true;
-                            }
-                            if (isProbableSolvable(i)) {
-                                if (solve(index + 1, true)) {
-                                    return true;
-                                }
-                            }
-                            remove(current, i, j, k, d);
-                        }
-                    }
-                }
+    if (nextRandom) {
+      int r = (int) (Math.random() * randomCandidates.length);
+      for (int ri = 0; ri < candidates.length; ri++) {
+        int current = randomCandidates[r][ri];
+        if (current == 2) {
+          // take an abbreviation
+          if (insert(current, i, j, k, 0)) {
+            if (isSolved()) {
+              return true;
             }
+            if (isProbableSolvable(i)) {
+              if (solve(index + 1, true)) {
+                return true;
+              }
+            }
+            remove(current, i, j, k, 0);
+          }
         } else {
-            for (int current = 0; current < candidates.length; current++) {
-                if (current == 2) {
-                    // take an abbreviation
-                    if (insert(current, i, j, k, 0)) {
-                        if (isSolved()) {
-                            return true;
-                        }
-                        if (isProbableSolvable(i)) {
-                            if (solve(index + 1, false)) {
-                                return true;
-                            }
-                        }
-                        remove(current, i, j, k, 0);
-                    }
-                } else {
-                    // take all directions
-                    for (int d = 0; d < 6; d++) {
-                        if (insert(current, i, j, k, d)) {
-                            if (isSolved()) {
-                                return true;
-                            }
-                            if (isProbableSolvable(i)) {
-                                if (solve(index + 1, false)) {
-                                    return true;
-                                }
-                            }
-                            remove(current, i, j, k, d);
-                        }
-                    }
+          // take all directions
+          for (int d = 0; d < 6; d++) {
+            if (insert(current, i, j, k, d)) {
+              if (isSolved()) {
+                return true;
+              }
+              if (isProbableSolvable(i)) {
+                if (solve(index + 1, true)) {
+                  return true;
                 }
+              }
+              remove(current, i, j, k, d);
             }
+          }
         }
-
-        // May we find a solution by leaving this index empty?
-        return solve(index + 1, nextRandom);
-    }
-
-    public void startSolving(final boolean nextRandom) {
-        solve(0, nextRandom);
-    }
-
-    public void printInfo() {
-        System.out.println("len             = " + len);
-        System.out.println("maxCandidates   = " + maxCandidates);
-        System.out.println("withMagicCuboid = " + withMagicCuboid);
-        System.out.println("candidates      = " + Arrays.deepToString(candidates));
-        System.out.println("available       = " + Arrays.toString(available));
-        System.out.println("isSolved        = " + isSolved());
-        System.out.println("solution        = ");
-        for (int[][] ints : solution) {
-            System.out.println(Arrays.deepToString(ints));
+      }
+    } else {
+      for (int current = 0; current < candidates.length; current++) {
+        if (current == 2) {
+          // take an abbreviation
+          if (insert(current, i, j, k, 0)) {
+            if (isSolved()) {
+              return true;
+            }
+            if (isProbableSolvable(i)) {
+              if (solve(index + 1, false)) {
+                return true;
+              }
+            }
+            remove(current, i, j, k, 0);
+          }
+        } else {
+          // take all directions
+          for (int d = 0; d < 6; d++) {
+            if (insert(current, i, j, k, d)) {
+              if (isSolved()) {
+                return true;
+              }
+              if (isProbableSolvable(i)) {
+                if (solve(index + 1, false)) {
+                  return true;
+                }
+              }
+              remove(current, i, j, k, d);
+            }
+          }
         }
+      }
     }
 
-    public static void solveAndPrint(final int maxCandidates, final boolean withMagicCuboid, final boolean nextRandom) {
-        System.out.println("Start solving with: maxCandidates = " + maxCandidates + ", withMagicCuboid = " + withMagicCuboid + ", nextRandom = " + nextRandom);
-        Box box = new Box(maxCandidates, withMagicCuboid);
-        box.startSolving(nextRandom);
-        box.printInfo();
-    }
+    // May we find a solution by leaving this index empty?
+    return solve(index + 1, nextRandom);
+  }
 
-    public static void main(final String[] args) {
-        solveAndPrint(15, true, true);
-        solveAndPrint(6, true, true);
+  public void startSolving(final boolean nextRandom) {
+    solve(0, nextRandom);
+  }
+
+  public void printInfo() {
+    System.out.println("len             = " + len);
+    System.out.println("maxCandidates   = " + maxCandidates);
+    System.out.println("withMagicCuboid = " + withMagicCuboid);
+    System.out.println("candidates      = " + Arrays.deepToString(candidates));
+    System.out.println("available       = " + Arrays.toString(available));
+    System.out.println("isSolved        = " + isSolved());
+    System.out.println("solution        = ");
+    for (int[][] ints : solution) {
+      System.out.println(Arrays.deepToString(ints));
     }
+  }
+
+  public static void solveAndPrint(
+      final int maxCandidates, final boolean withMagicCuboid, final boolean nextRandom) {
+    System.out.println(
+        "Start solving with: maxCandidates = "
+            + maxCandidates
+            + ", withMagicCuboid = "
+            + withMagicCuboid
+            + ", nextRandom = "
+            + nextRandom);
+    Box box = new Box(maxCandidates, withMagicCuboid);
+    box.startSolving(nextRandom);
+    box.printInfo();
+  }
+
+  public static void main(final String[] args) {
+    solveAndPrint(15, true, true);
+    solveAndPrint(6, true, true);
+  }
 }
--- src/main/java/Render.java
+++ src/main/java/Render.java
@@ -1,61 +1,76 @@
-import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
 
 public class Render {
-    private static Color getColor(final int index, final int level) {
-        float a = (5 - level) / 5f;
-        return switch (index) {
-            case 0 -> new Color(1f, 0.5f, 0f, a);
-            case 1 -> new Color(1f, 1f, 0f, a);
-            case 2 -> new Color(0f, 0f, 1f, a);
-            default -> null;
-        };
-    }
+  private static Color getColor(final int index, final int level) {
+    float a = (5 - level) / 5f;
+    return switch (index) {
+      case 0 -> new Color(1f, 0.5f, 0f, a);
+      case 1 -> new Color(1f, 1f, 0f, a);
+      case 2 -> new Color(0f, 0f, 1f, a);
+      default -> null;
+    };
+  }
 
-    public Render(final int[][][] solution, final int maxCandidates) {
-        int widthAndHeight = 800;
+  public Render(final int[][][] solution, final int maxCandidates) {
+    int widthAndHeight = 800;
 
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(widthAndHeight, widthAndHeight);
+    JFrame frame = new JFrame();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(widthAndHeight, widthAndHeight);
 
-        frame.add(new Canvas() {
-            @Override
-            public void paint(Graphics g) {
-                g.setColor(Color.BLACK);
-                g.fillRect(0, 0, getWidth(), getHeight());
+    frame.add(
+        new Canvas() {
+          @Override
+          public void paint(Graphics g) {
+            g.setColor(Color.BLACK);
+            g.fillRect(0, 0, getWidth(), getHeight());
 
-                int len = 150;
-                int len2 = 15;
-                int len3 = 140;
-                for (int i = 4; i >= 0; i--) {
-                    for (int j = 0; j < 5; j++) {
-                        for (int k = 0; k < 5; k++) {
-                            int x = k * len3 + i * len2 + (getWidth() - 695) / 2;
-                            int y = j * len3 + i * len2 + (getHeight() - 695) / 2;
-                            int x2 = x + len / 2;
-                            int y2 = y + len / 2;
-                            Color color = getColor(solution[i][j][k] / maxCandidates, i);
-                            g.setColor(color);
-                            g.fillRect(x, y, x2 - x, y2 - y);
-                            g.setColor(Color.GRAY);
-                            g.drawString(String.valueOf(solution[i][j][k]), x2 - 15, y + 10);
-                        }
-                    }
+            int len = 150;
+            int len2 = 15;
+            int len3 = 140;
+            for (int i = 4; i >= 0; i--) {
+              for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 5; k++) {
+                  int x = k * len3 + i * len2 + (getWidth() - 695) / 2;
+                  int y = j * len3 + i * len2 + (getHeight() - 695) / 2;
+                  int x2 = x + len / 2;
+                  int y2 = y + len / 2;
+                  Color color = getColor(solution[i][j][k] / maxCandidates, i);
+                  g.setColor(color);
+                  g.fillRect(x, y, x2 - x, y2 - y);
+                  g.setColor(Color.GRAY);
+                  g.drawString(String.valueOf(solution[i][j][k]), x2 - 15, y + 10);
                 }
+              }
             }
+          }
         });
 
-        frame.setVisible(true);
-    }
+    frame.setVisible(true);
+  }
 
-    public static void main(final String[] args) {
-        Render render = new Render(new int[][][]{
-                {{13, 6, 6, 6, 6}, {7, 6, 6, 6, 6}, {7, 0, 0, 1, 1}, {7, 0, 0, 1, 1}, {7, 0, 0, 8, 8}},
-                {{9, 9, 2, 2, 2}, {7, 14, 2, 2, 2}, {7, 0, 0, 1, 1}, {7, 0, 0, 1, 1}, {7, 0, 0, 8, 8}},
-                {{9, 9, 2, 2, 2}, {3, 3, 2, 2, 2}, {3, 3, 12, 1, 1}, {4, 4, 4, 1, 1}, {4, 4, 4, 8, 8}},
-                {{9, 9, 5, 5, 10}, {3, 3, 5, 5, 10}, {3, 3, 5, 5, 10}, {4, 4, 4, 15, 10}, {4, 4, 4, 8, 8}},
-                {{9, 9, 5, 5, 10}, {3, 3, 5, 5, 10}, {3, 3, 5, 5, 10}, {11, 11, 11, 11, 10}, {11, 11, 11, 11, 16}},
-        }, 6);
-    }
+  public static void main(final String[] args) {
+    new Render(
+        new int[][][] {
+          {{13, 6, 6, 6, 6}, {7, 6, 6, 6, 6}, {7, 0, 0, 1, 1}, {7, 0, 0, 1, 1}, {7, 0, 0, 8, 8}},
+          {{9, 9, 2, 2, 2}, {7, 14, 2, 2, 2}, {7, 0, 0, 1, 1}, {7, 0, 0, 1, 1}, {7, 0, 0, 8, 8}},
+          {{9, 9, 2, 2, 2}, {3, 3, 2, 2, 2}, {3, 3, 12, 1, 1}, {4, 4, 4, 1, 1}, {4, 4, 4, 8, 8}},
+          {
+            {9, 9, 5, 5, 10},
+            {3, 3, 5, 5, 10},
+            {3, 3, 5, 5, 10},
+            {4, 4, 4, 15, 10},
+            {4, 4, 4, 8, 8}
+          },
+          {
+            {9, 9, 5, 5, 10},
+            {3, 3, 5, 5, 10},
+            {3, 3, 5, 5, 10},
+            {11, 11, 11, 11, 10},
+            {11, 11, 11, 11, 16}
+          },
+        },
+        6);
+  }
 }
